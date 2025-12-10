e81050f3646422bf086e6c6fed1cb76608f5207e
kareemEG14 <karimemil14@gmail.com>
2025-12-09 18:30:04 -0800

    Fixed config for pins

--- config.h
+++ config.h
@@ -4,8 +4,6 @@
 const int MOTOR_PWM_PIN   = 5;
 const int MOTOR_RPM_PIN  = 2;
 const int THRUST_ADC_PIN  = A0;
-const int VOLTAGE_READ_PIN = A1;
-const int CURRENT_READ_PIN = A2;
 // ---------------- POWER CONFIG ----------------
 
 // ESP32 ADC channels for voltage + current
